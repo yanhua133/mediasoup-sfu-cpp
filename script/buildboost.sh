05d1be90c98bcbe3e196a64ad25f507bfadab486
yanhua133 <yanhua133@126.com>
2021-09-17 11:25:39 +0800

    linux boost compile shell fix

--- script/buildboost.sh
+++ script/buildboost.sh
@@ -1,10 +1,10 @@
 cur_dir=$(cd "$(dirname "$0")"; pwd)
 parent_dir=$(dirname $(pwd))
 
-if [ -e deps/boost/boost_1_74_0.tar.gz ]; then
+if [ -e ${parent_dir}/deps/boost/boost_1_74_0.tar.gz ]; then
    echo "exist deps/boost/boost_1_74_0.tar.gz"
 else
-   /bin/cp -rf lib/boost_1_74_0.tar.gz  deps/boost/boost_1_74_0.tar.gz
+   /bin/cp -rf ${parent_dir}/lib/boost_1_74_0.tar.gz  ${parent_dir}/deps/boost/boost_1_74_0.tar.gz
 fi
 
 cd ${parent_dir}/deps/boost/
