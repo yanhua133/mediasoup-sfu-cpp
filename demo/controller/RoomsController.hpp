201b0050cada4ab08620c2d60766556e5fa74ab1
gabys999 <gabys999@gmail.com>
2021-12-20 15:36:36 +0800

    add subprotocol
    add subprotocol

--- demo/controller/RoomsController.hpp
+++ demo/controller/RoomsController.hpp
@@ -37,8 +37,9 @@
 #include "oatpp/core/macro/codegen.hpp"
 #include "oatpp/core/macro/component.hpp"
 
-
 #include OATPP_CODEGEN_BEGIN(ApiController) /// <-- Begin Code-Gen
+
+#define WS_SUBPROTOCOL "protoo"
 
 class RoomsController : public oatpp::web::server::api::ApiController {
 private:
@@ -50,33 +51,39 @@ public:
     : oatpp::web::server::api::ApiController(objectMapper)
   {}
 public:
-
+   
   //this._protooUrl = `ws://192.168.0.106:8001/?roomId=${roomInfo.roomId}&peerId=${roomInfo.userId}&forceH264=true&forceVP9=false`
   ENDPOINT_ASYNC("GET", "/*", WS) {
 
     ENDPOINT_ASYNC_INIT(WS)
 
     Action act() override {
+        //HEADER(String, subprotocol, "Sec-WebSocket-Protocol");
+        String subprotocol = request->getHeader("Sec-WebSocket-Protocol");
+        OATPP_ASSERT_HTTP(subprotocol == WS_SUBPROTOCOL, Status::CODE_400, "unknown subprotocol")
+        
         auto roomId = request->getQueryParameter("roomId","123456");
         auto peerId = request->getQueryParameter("peerId","123456");
         auto forceH264 = request->getQueryParameter("forceH264","false"); //type is oatpp::string should convert to bool
         auto forceVP9 = request->getQueryParameter("forceVP9","false");
-      /* Websocket handshake */
-      MS_lOGD("[Room] new connection roomId=%s peerId=%s",roomId->c_str(), peerId->c_str());
-      auto response = oatpp::websocket::Handshaker::serversideHandshake(request->getHeaders(), controller->websocketConnectionHandler);
 
-      auto parameters = std::make_shared<oatpp::network::ConnectionHandler::ParameterMap>();
+        /* Websocket handshake */
+        MS_lOGD("[Room] new connection roomId=%s peerId=%s", roomId->c_str(), peerId->c_str());
+        auto response = oatpp::websocket::Handshaker::serversideHandshake(request->getHeaders(), controller->websocketConnectionHandler);
 
-      (*parameters)["roomId"] = roomId;
-      (*parameters)["peerId"] = peerId;
-      (*parameters)["forceH264"] = forceH264;
-      (*parameters)["forceVP9"] = forceVP9;
+        auto parameters = std::make_shared<oatpp::network::ConnectionHandler::ParameterMap>();
 
-      /* Set connection upgrade params */
-      response->setConnectionUpgradeParameters(parameters);
+        (*parameters)["roomId"] = roomId;
+        (*parameters)["peerId"] = peerId;
+        (*parameters)["forceH264"] = forceH264;
+        (*parameters)["forceVP9"] = forceVP9;
 
-      return _return(response);
+        /* Set connection upgrade params */
+        response->setConnectionUpgradeParameters(parameters);
 
+        response->putHeader("Sec-WebSocket-Protocol", WS_SUBPROTOCOL);
+
+        return _return(response);
     }
 
   };
